b907b4c0d8e62e835366ef490bf1a3b4a0f12807
juan <juan>
2023-05-23 17:38:43 -0500

    actualizacion tablas de la base de datos

--- reservaEventos.sql
+++ reservaEventos.sql
@@ -9,3 +9,16 @@ CREATE TABLE reservas (
   time INT NOT NULL,
   attendees INT NOT NULL
 );
+
+CREATE TABLE lugares (
+  id varchar(25) PRIMARY KEY,
+  direccion VARCHAR(255) NOT NULL,
+  tipo_lugar varchar(255) NOT NULL
+);
+
+CREATE TABLE usuarios (
+  id_usuario varchar(25) PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  correo VARCHAr(255) NOT NULL,
+  numero varchar(255) NOT NULL
+);

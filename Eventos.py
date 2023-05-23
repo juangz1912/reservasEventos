e5d57b71c603b31d436d7265ef99c2e6cab39f7a
juan <juan>
2023-05-23 11:52:20 -0500

    se agrego logo udem

--- Eventos.py
+++ Eventos.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 from tkinter import ttk
 import datetime
 import mysql.connector
-
+from PIL import ImageTk, Image
 
 class Database:
     def __init__(self):
@@ -91,10 +91,7 @@ class Database:
                 return [
                     {
                         'id': reservation[0],
-                        'event_name': reservation[1],
-                        'date': reservation[2],
-                        'time': reservation[3],
-                        'attendees': reservation[4]
+                        'event_name': reservation[1]
                     }
                     for reservation in reservations
                 ]
@@ -119,20 +116,26 @@ class EventReservationApp:
         self.attendees_var = tk.StringVar()
 
         # Elementos de la interfaz
-        self.id_label = ttk.Label(root, text="ID:")
+        self.id_label = tk.Label(root, text="ID:")
         self.id_entry = ttk.Entry(root, textvariable=self.id_var)
-        self.event_name_label = ttk.Label(root, text="Nombre del evento:")
+        self.event_name_label = tk.Label(root, text="Nombre del evento:")
         self.event_name_entry = ttk.Entry(root, textvariable=self.event_name_var)
-        self.date_label = ttk.Label(root, text="Fecha (dd/mm/aaaa):")
+        self.date_label = tk.Label(root, text="Fecha (dd/mm/aaaa):")
         self.date_entry = ttk.Entry(root, textvariable=self.date_var)
-        self.time_label = ttk.Label(root, text="Hora (hh:mm):")
+        self.time_label = tk.Label(root, text="Hora (hh:mm):")
         self.time_entry = ttk.Entry(root, textvariable=self.time_var)
-        self.attendees_label = ttk.Label(root, text="Asistentes:")
+        self.attendees_label = tk.Label(root, text="Asistentes:")
         self.attendees_entry = ttk.Entry(root, textvariable=self.attendees_var)
         self.add_button = tk.Button(root, text="Agregar", command=self.add_reservation)
         self.search_button = tk.Button(root, text="Buscar", command=self.search_reservation)
         self.delete_button = tk.Button(root, text="Eliminar", command=self.delete_reservation)
         self.view_all_button = tk.Button(root, text="Ver todos", command=self.view_all_reservations)
+
+        # Cargar la imagen
+        self.image = Image.open("/Users/juanjo/Desktop/udem.png")
+        self.image = self.image.resize((100, 100), Image.LANCZOS)
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.image_label = tk.Label(root, image=self.photo)
 
         # Posicionamiento de los elementos
         self.id_label.grid(row=0, column=0, sticky=tk.W, padx=10, pady=5)
@@ -149,6 +152,7 @@ class EventReservationApp:
         self.search_button.grid(row=5, column=1, padx=10, pady=5)
         self.delete_button.grid(row=5, column=2, padx=10, pady=5)
         self.view_all_button.grid(row=5, column=3, padx=10, pady=5)
+        self.image_label.grid(row=0, column=4, rowspan=4, padx=10, pady=5, sticky=tk.NE)
 
         # Conexión a la base de datos
         self.database = Database()
@@ -156,90 +160,53 @@ class EventReservationApp:
     def add_reservation(self):
         id = self.id_var.get()
         event_name = self.event_name_var.get()
-        date_str = self.date_var.get()
-        time_str = self.time_var.get()
+        date = self.date_var.get()
+        time = self.time_var.get()
         attendees = self.attendees_var.get()
 
-        if id and event_name and date_str and time_str and attendees:
-            try:
-                date_obj = datetime.datetime.strptime(date_str, "%d/%m/%Y")
-                date_int = date_obj.strftime("%Y%m%d")
+        reservation = {
+            'id': id,
+            'event_name': event_name,
+            'date': date,
+            'time': time,
+            'attendees': attendees
+        }
 
-                time_obj = datetime.datetime.strptime(time_str, "%H:%M")
-                time_int = time_obj.strftime("%H%M")
-
-                reservation = {
-                    'id': id,
-                    'event_name': event_name,
-                    'date': date_int,
-                    'time': time_int,
-                    'attendees': attendees
-                }
-
-                self.database.insert_reservation(reservation)
-
-                self.clear_entries()
-            except ValueError:
-                messagebox.showerror("Error", "Fecha u hora inválida. Utiliza el formato correcto.")
-        else:
-            messagebox.showwarning("Advertencia", "Por favor completa todos los campos.")
+        self.database.insert_reservation(reservation)
 
     def search_reservation(self):
         event_name = self.event_name_var.get()
 
-        if event_name:
-            reservation = self.database.get_reservation_by_event_name(event_name)
+        reservation = self.database.get_reservation_by_event_name(event_name)
 
-            if reservation:
-                messagebox.showinfo("Reserva encontrada", f"ID: {reservation['id']}\n"
-                                                           f"Nombre del evento: {reservation['event_name']}\n"
-                                                           f"Fecha: {reservation['date']}\n"
-                                                           f"Hora: {reservation['time']}\n"
-                                                           f"Asistentes: {reservation['attendees']}")
-            else:
-                messagebox.showinfo("Reserva no encontrada", "No se encontró ninguna reserva con ese nombre de evento.")
+        if reservation:
+            messagebox.showinfo("Reserva Encontrada", f"ID: {reservation['id']}\n"
+                                                      f"Nombre del evento: {reservation['event_name']}\n"
+                                                      f"Fecha: {reservation['date']}\n"
+                                                      f"Hora: {reservation['time']}\n"
+                                                      f"Asistentes: {reservation['attendees']}")
         else:
-            messagebox.showwarning("Advertencia", "Por favor ingresa el nombre del evento a buscar.")
+            messagebox.showinfo("Reserva no encontrada", "No se encontró una reserva con ese nombre de evento.")
 
     def delete_reservation(self):
         event_name = self.event_name_var.get()
 
-        if event_name:
-            confirmation = messagebox.askyesno("Confirmación", "¿Estás seguro/a de que deseas eliminar la reserva?")
-
-            if confirmation:
-                self.database.delete_reservation_by_event_name(event_name)
-                self.clear_entries()
-        else:
-            messagebox.showwarning("Advertencia", "Por favor ingresa el nombre del evento a eliminar.")
+        self.database.delete_reservation_by_event_name(event_name)
 
     def view_all_reservations(self):
         reservations = self.database.get_all_reservations()
 
         if reservations:
-            messagebox.showinfo("Todas las reservas", "\n".join(
-                f"ID: {reservation['id']}, Nombre del evento: {reservation['event_name']}, "
-                f"Fecha: {reservation['date']}, Hora: {reservation['time']}, Asistentes: {reservation['attendees']}"
-                for reservation in reservations
-            ))
+            messagebox.showinfo("Todas las Reservas", "\n".join([f"ID: {reservation['id']}, "
+                                                                 f"Nombre del evento: {reservation['event_name']}"
+                                                                 for reservation in reservations]))
         else:
-            messagebox.showinfo("Todas las reservas", "No se encontraron reservas.")
+            messagebox.showinfo("No hay reservas", "No se encontraron reservas en la base de datos.")
 
-    def clear_entries(self):
-        self.id_var.set("")
-        self.event_name_var.set("")
-        self.date_var.set("")
-        self.time_var.set("")
-        self.attendees_var.set("")
+    def run(self):
+        self.root.mainloop()
 
 
 root = tk.Tk()
 app = EventReservationApp(root)
-root.mainloop()
-
-
-
-
-
-
-
+app.run()
